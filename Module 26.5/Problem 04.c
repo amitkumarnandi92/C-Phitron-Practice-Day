1d419da08d43c367cb030fd3399a3fee6071bb56
amitkumarnandi628 <amitkumarnandi92@gmail.com>
2023-03-11 10:34:11 +0600

    added Problem 04

---
+++ Module 26.5/Problem 04.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+float aoc(float);
+int main()
+{
+    float r,result;
+    printf("Enter radius of a circle: ");
+    scanf("%f",&r);
+    result=aoc(r);
+    printf("The area of circle is %f",result);
+    return 0;
+}
+float aoc(float q)
+{
+    float w,pi;
+    pi=3.14;
+    w=pi*q*q;
+    return w;
+}
